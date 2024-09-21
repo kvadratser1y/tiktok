d1c4b49f69207a28a084fc386f76248dfdc50034
kvadratser1y <kvadratseriy@icloud.com>
2024-09-21 16:41:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+// Перенаправление через 2 секунды
+window.onload = function() {
+    setTimeout(function() {
+        window.location.href = "https://www.tiktok.com";
+    }, 2000); // 2000 миллисекунд = 2 секунды
+};
